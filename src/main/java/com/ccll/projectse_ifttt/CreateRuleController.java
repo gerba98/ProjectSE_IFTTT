f48a492ec1bfeae9af25c2c65afccab67ce19e96
CGraziano16 <c.graziano16@studenti.unisa.it>
2024-10-12 17:25:31 +0200

    implementation creation rule window

---
+++ src/main/java/com/ccll/projectse_ifttt/CreateRuleController.java
@@ -0,0 +1,51 @@
+package com.ccll.projectse_ifttt;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class CreateRuleController {
+
+    @FXML
+    private Button createButton;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private TextField ruleNameTxtField;
+    @FXML
+    private ComboBox<String> triggerBox;
+    @FXML
+    private ComboBox<String> actionBox;
+    @FXML
+    private ObservableList<String> triggersList = FXCollections.observableArrayList("Time of the Day","Day of the week","Day of the month","Date","File existence","File dimension","Status program");
+    @FXML
+    private ObservableList<String> actionsList = FXCollections.observableArrayList("Display message","Play Audio","Write string","Copy File","Move file","Remove file","Execute Program");
+
+    public void initialize(){
+        triggerBox.setItems(triggersList);
+        actionBox.setItems(actionsList);
+    }
+
+    public void onCreateButtonClick(ActionEvent actionEvent) {
+        String name = ruleNameTxtField.getText();
+        String trigger = triggerBox.getValue();
+        String action = actionBox.getValue();
+
+        //new Rule();
+
+        Stage stage = (Stage) createButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void onClearButtonClick(ActionEvent actionEvent) {
+        ruleNameTxtField.clear();
+        triggerBox.setValue(null);
+        actionBox.setValue(null);
+    }
+}
+
--- src/main/java/com/ccll/projectse_ifttt/FXMLDocumentController.java
+++ src/main/java/com/ccll/projectse_ifttt/FXMLDocumentController.java
@@ -1,10 +1,18 @@
 package com.ccll.projectse_ifttt;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class FXMLDocumentController {
 
@@ -21,20 +29,30 @@ public class FXMLDocumentController {
     @FXML
     private Button CancelCreationButton;
 
-    @FXML
-    private Pane RulePaneCreation;
+
 
 
     @FXML
     public void initialize()
     {
-        RulePaneCreation.setVisible(false);
+        //RulePaneCreation.setVisible(false);
     }
 
     @FXML
     public void OnCreateRuleClick()
     {
-        RulePaneCreation.setVisible(!RulePaneCreation.isVisible());
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("create-rule.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setTitle("Create Rule");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }catch(IOException ex){
+            System.out.println("I/O error");
+        }
     }
 
 }
