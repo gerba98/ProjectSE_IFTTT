1379fe3b7da8ba4116bf827aa2fb1aeda330b1a0
giggiKush <cuozzoluigi38@gmail.com>
2024-10-12 19:55:38 +0200

    implementazione elmenti dinamici per tipi di trigger

--- src/main/java/com/ccll/projectse_ifttt/CreateRuleController.java
+++ src/main/java/com/ccll/projectse_ifttt/CreateRuleController.java
@@ -1,16 +1,25 @@
 package com.ccll.projectse_ifttt;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class CreateRuleController {
 
+    @FXML
+    private Pane rulePane;
+    @FXML
+    private Label labelTriggerSelected;
     @FXML
     private Button createButton;
     @FXML
@@ -28,6 +37,9 @@ public class CreateRuleController {
     @FXML
     private FXMLDocumentController fxmlDocumentController;
 
+    private ObservableList<Object> paneItems = FXCollections.observableArrayList();
+
+
     public void setFXMLDocumentController(FXMLDocumentController fxmlDocumentController) {
         this.fxmlDocumentController = fxmlDocumentController;
     }
@@ -35,6 +47,52 @@ public class CreateRuleController {
     public void initialize(){
         triggerBox.setItems(triggersList);
         actionBox.setItems(actionsList);
+
+        triggerBox.setOnAction(e -> {
+            String selectedItem = triggerBox.getSelectionModel().getSelectedItem();
+            if(paneItems.size() > 0)
+            {
+                rulePane.getChildren().remove(paneItems.getFirst());
+                paneItems.remove(0);
+            }
+            createItem(labelTriggerSelected, selectedItem);
+        });
+
+    }
+
+    private void createItem(Label label, String text)
+    {
+        switch (text) {
+            case "Time of the Day":
+                label.setText("Time of the Day (hh:mm)");
+
+                TextField timeField = new TextField();
+                timeField.setPromptText("hh:mm");
+                timeField.setLayoutX(285.0);
+                timeField.setLayoutY(142.0);
+
+                rulePane.getChildren().add(timeField);
+                paneItems.add(timeField);
+
+                break;
+            case "Day of the week":
+                label.setText("Day of the week (Monday, etc..)");
+
+                ComboBox<String> dayOfWeek = new ComboBox<String>();
+                ObservableList<String> week = FXCollections.observableArrayList("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+                dayOfWeek.setItems(week);
+
+                dayOfWeek.setLayoutX(285.0);
+                dayOfWeek.setLayoutY(142.0);
+
+                rulePane.getChildren().add(dayOfWeek);
+                paneItems.add(dayOfWeek);
+
+                break;
+            case "Day of the month":
+                label.setText("Day of the month (1, 2, etc..)");
+                break;
+        }
     }
 
     public void onCreateButtonClick(ActionEvent actionEvent) {
