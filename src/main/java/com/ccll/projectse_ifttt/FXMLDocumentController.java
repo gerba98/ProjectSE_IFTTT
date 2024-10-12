d9ca300b3a2df9ced79769e8e8c1969017270213
giggiKush <cuozzoluigi38@gmail.com>
2024-10-12 16:14:58 +0200

    new branch GUI

--- src/main/java/com/ccll/projectse_ifttt/FXMLDocumentController.java
+++ src/main/java/com/ccll/projectse_ifttt/FXMLDocumentController.java
@@ -1,14 +1,40 @@
 package com.ccll.projectse_ifttt;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class FXMLDocumentController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button CreateRuleButton;
+
+    @FXML
+    private Button DeleteRuleButton;
+
+    @FXML
+    private Button SaveRuleButton;
+
+    @FXML
+    private Button CancelCreationButton;
+
+    @FXML
+    private Pane RulePaneCreation;
+
+
+    @FXML
+    public void initialize()
+    {
+        RulePaneCreation.setVisible(false);
     }
+
+    @FXML
+    public void OnCreateRuleClick()
+    {
+        RulePaneCreation.setVisible(!RulePaneCreation.isVisible());
+    }
+
 }
